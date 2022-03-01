91d5ecd49518916f4b5704aae89a7473c8d27f1f
HabibOnTheGo <HabibOnTheGo12@gmail.com>
2022-03-01 21:10:13 +0600

    done all sestem

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,10 @@
 const PhoneSearchResult = () =>{
     const inputText = document.getElementById('input-text');
     const inputValue = inputText.value;
-    const url =``
+    const url =`https://openapi.programming-hero.com/api/phones?search=${inputValue}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data.data))
 
 }
+const 
