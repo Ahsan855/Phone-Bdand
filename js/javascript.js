9df49f9ffd049079fdeaea367bd634fb1f2c6875
HabibOnTheGo <HabibOnTheGo12@gmail.com>
2022-03-02 21:47:17 +0600

    done all features

--- js/javascript.js
+++ js/javascript.js
@@ -17,8 +17,8 @@ const mobileReviwe = phones => {
         <div class="card">
           <img src="${phone.image}" class="card-img-top" alt="...">
          <div class="card-body">
-          <h5 class="card-title">name${phone.brand}}</h5>
-          <h5 class="card-title">name${phone.phone_name}}</h5>
+          <h5 class="card-title">name ${phone.brand}</h5>
+          <h5 class="card-title">name ${phone.phone_name}</h5>
           <a onclick="detailsAllPhone('${phone.slug}')" class="btn btn-primary">Details</a>
          </div>
         </div>`;
@@ -29,28 +29,47 @@ const mobileReviwe = phones => {
 
 }
 
-const detailsAllPhone = phone =>{
-// console.log(phone)
-const url =`https://openapi.programming-hero.com/api/phone/${phone}`
-fetch(url)
-.then(res => res.json())
-.then(optimize => showButton(optimize.data))
+const detailsAllPhone = id => {
+    // console.log(phone)
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(optimize => showButton(optimize.data))
 }
 
 
-const showButton = mobile =>{
-   console.log(mobile)
-   const show=document.getElementById('showDetails')
-   const div = document.createElement('div');
-        div.classList.add('col-md-4');
-        div.innerHTML = `
-        <div class="card">
-          <img src="${mobile.mainfeature.img}" class="card-img-top" alt="...">
+const showButton = mobile => {
+    console.log(mobile)
+    const show = document.getElementById('showDetails')
+    const div = document.createElement('div');
+    div.classList.add('col-12');
+    console.log(mobile.mainFeatures.chipSet);
+    div.innerHTML = `
+        <div class="row mb-5 p-3">
+          <div class="col-md-4">
+          <img src="${mobile.image}" class="card-img-top" alt="...">
+          </div>
+          <div class="col-md-8">
+          <div class="card">
          <div class="card-body">
-         <h5 class="card-title">name${phone.brand}}</h5>
-          <h5 class="card-title">name${mobile.Mainfeature.nam}}</h5>
-          <a onclick="detailsAllPhone('${phone.slug}')" class="btn btn-primary">Details</a>
+         <h3 class="card-title">MainFeature</h3>
+          <h3 class="card-title">ChipSet:${mobile.mainFeatures.chipSet}</h3>
+          <p><strong>DisplaySize: </strong>${mobile.mainFeatures.displaySize}</p>
+          <p><strong>Memory: </strong>${mobile.mainFeatures.memory}</p>
+          <p><strong>Storage: </strong>${mobile.mainFeatures.storage}</p>
+          <p><strong>Sensors: </strong>${mobile.mainFeatures.sensors}</p>
+          <h3 class="card-title">Others Features</h3>
+          <p><strong>Bluetooth: </strong>${mobile.others.Bluetooth}</p>
+          <p><strong>GPS: </strong>${mobile.others.GPS}</p>
+          <p><strong>NFC: </strong>${mobile.others.NFC}</p>
+          <p><strong>Radio: </strong>${mobile.others.Radio}</p>
+          <p><strong>USB: </strong>${mobile.others.USB}</p>
+          <p><strong>WLAN: </strong>${mobile.others.WLAN}</p>
          </div>
-        </div>`;
-        show.appendChild(div);
+        </div>
+          </div>
+          
+        </div>
+    `;
+    show.appendChild(div);
 }
